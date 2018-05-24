b5b4e515cd462be5c3cd882c5d2ffe28f4c5301c
Sterling Moss <mtsilvertm@gmail.com>
2018-05-24 14:01:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string) lowercase;
+  if (string = lowercase);
     return "I can't hear you!";
-  if (string) uppercase; 
+  if (string = uppercase); 
     return "YES INDEED!";
-  if (string) mixedCase;
+  if (string = mixedCase);
     return "I love you, too.";
 }
